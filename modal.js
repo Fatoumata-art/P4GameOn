f97632b4cb7b86e8ae169cde50838b5adcea9d19
Fatoumata <85653301+Fatoumata-art@users.noreply.github.com>
2023-02-14 13:49:10 +0100

    function close modal

--- modal.js
+++ modal.js
@@ -13,30 +13,40 @@ const modalbg2 = document.getElementById("modalbg2")
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtn2 = document.querySelectorAll(".modal-btn2");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll("[data-close-modal]");
 
 var validalidation = document.getElementById('send_button');
 var fname = document.getElementById('first-name');
 var m_fname = document.getElementById('missing_fname');
+var l_name = document.getElementById('last-name');
+var m_l_name = document.getElementById('missing_last-name');
+var email = document.getElementById('email');
+var m_email = document.getElementById('missing_email');
+var ddn = document.getElementById('birthdate');
+var m_ddn = document.getElementById('missing_birthdate');
+
+var nameRGEX = 
 
 // launch modal event
 validalidation.addEventListener("submit", validateForm);
 modalBtn.forEach((btn) => {btn.addEventListener("click", launchModal)});
-// modalBtn2.forEach((btn) => btn.addEventListener("click", launchModal2));
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// function launchModal2() {
-//   modalbg2.style.display = "block";
-// }
+function closeModal(){
+  modalbg.style.display = "none";
+  resetModal();
+}
+
 
 // form validalidation
 
 function validateForm(e){
-  console.log('JJJJJJJJJJJJJJJJJJJJJJJJJJJ')
-  e.preventDefault();
-  
   if (fname.value === ''|| fname.value == false){
     // bloquer l'envoi du form
     e.preventDefault();
