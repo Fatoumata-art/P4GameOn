515149ee05f198329b16873d40ab1aa2dd87a82a
Fatoumata <85653301+Fatoumata-art@users.noreply.github.com>
2023-02-20 12:40:28 +0100

    change event inplemented

--- modal.js
+++ modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myHeader");
+  let x = document.getElementById("myHeader");
   if (x.className === "header") {
     x.className += " responsive";
   } else {
@@ -15,94 +15,129 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const validalidation = document.forms["reserve"];
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
-var msgError = document.getElementById('msgError')
-var fname = document.getElementById("fname");
-var l_name = document.getElementById("last");
-var email = document.getElementById("email");
-var birthdate = document.getElementById("birthdate");
-var nberQuantity = document.getElementById("quantity");
+let msgError = document.getElementById('msgError')
+let fname = document.getElementById("fname");
+let l_name = document.getElementById("last");
+let email = document.getElementById("email");
+let birthdate = document.getElementById("birthdate");
+let nberQuantity = document.getElementById("quantity");
+let BtnRadio = document.getElementsByName('location');
+let CheckCondition = document.getElementById('checkbox1');
+let valid = false;
 
-// Task #2 Finaliser le formulaire //
-// Les champs autres que radio et checkbox sont contrôlés par HTML5
-// Déclaration des éleménts DOM utilisés
-const BtnRadio = document.getElementsByName('location');
-const CheckCondition = document.getElementById('checkbox1');
 
 // launch modal event
 modalBtn.forEach((btn) => {btn.addEventListener("click", launchModal)});
-
-//Task #1 - Evénement 'click' sur bouton fermeture
-closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-validalidation.addEventListener("submit", e => {
-  e.preventDefault();
-
-  validateForm();
-});
-  
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//Task #1 - Evénement 'click' sur bouton fermeture
+closeBtn.forEach((btn) => {btn.addEventListener("click", closeModal)});
 //Task #1 - Evénement 'click' sur bouton fermeture
 function closeModal(){
   modalbg.style.display = "none";
  
 }
 
+// function display msg error
 const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.msgError');
-
+  const formData =  element.parentElement
+  const errorDisplay = formData.querySelector('.msgError');
   errorDisplay.innerHTML  = message;
-  inputControl.classList.add("error")
-  inputControl.classList.remove("success")
+  formData.classList.add("error")
+  formData.classList.remove("success")
 }
-
+// function input success
 const setSuccess = element => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.msgError');
-
+  const formData =  element.parentElement
+  const errorDisplay = formData.querySelector('.msgError');
   errorDisplay.innerHTML  = '';
-  inputControl.classList.add("success")
-  inputControl.classList.remove("error")
-
-  
+  formData.classList.add("success")
+  formData.classList.remove("error")
 }
 
-function validateForm(){
-  const fnameValue = fname.value.trim();
-  const l_nameValue = l_name.value.trim();
-  const emailValue = email.value.trim();
-  const birthdateValue = birthdate.value.trim();
-  const nberQuantityValue = nberQuantity.value.trim();
-
-  if(fnameValue === ''){
-    setError(fname, 'Veiller renseigner le Prénom');
-  }else{
+// control entre de prénom
+fname.addEventListener("change", (e) => {
+  if(fname.value == ''){
+    setError(fname, 'Veuillez renseigner le Prénom');
+    e.preventDefault();
+}
+else if(fname.value.length <=2){
+  setError(fname, 'Veuillez entrer 2 caractères ou plus pour le champ du Prénom.')
+  e.preventDefault();
+}else{
     setSuccess(fname);
-  }
-
-  if(l_nameValue === ''){
-    setError(l_name, 'Veiller renseigner le nom');
+    valid = true;
 }
-else if(!validerName(l_nameValue)){
-  setError(l_name, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+})
+
+// control entre du nom
+l_name.addEventListener("change", (e) => {
+  if(l_name.value == ''){
+    setError(l_name, 'Veuillez renseigner le Nom');
+    e.preventDefault();
 }else{
     setSuccess(l_name);
-  }
+    valid = true;
+}
+})
 
-  if(emailValue === ''){
-    setError(email, 'Veiller renseigner le email');
-  }else if(!validerEmail(emailValue)){
-    setError(email, 'Veuillez entrer une adresse e-mail valide')
+// control entre de prénom
+function validEmail(email) {
+  let regexName = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regexName.test(email);
+};
+email.addEventListener("change", (e) => {
+  console.log(email.value);
+  if(email.value == ''){
+    setError(email, 'Veuillez renseigner votre email');
+    e.preventDefault();
   }
-  else{
-    setSuccess(email);
+ else if (!validEmail(email.value) ){
+    setError(email, 'Veuillez entrer une adresse e-mail valide.')
+    e.preventDefault();
+  }else{
+      setSuccess(email);
+      valid = true;
   }
+  })
+
+  // control entre de date de naissance
+  birthdate.addEventListener("change", (e) => {
+    if (!birthdate.validity.valid) {
+      setError(birthdate, 'Veuillez entrer une date valide');
+    }else{
+      setSuccess(birthdate);
+      valid = true;
+    }
+    })
+
+    // control entre du nombre de participation
+    nberQuantity.addEventListener("change", (e) => {
+  if(nberQuantity.value == ''){
+    setError(nberQuantity, 'Veuillez entrer une valeur entre 0 et 99');
+    e.preventDefault();
+}else{
+    setSuccess(nberQuantity);
+    valid = true;
+}
+})
+
+
+//function form validation
+function validateForm(){
+ 
+  if(l_nameValue === ''){
+    setError(l_name, 'Veuillez renseigner le Prénom');
+  }else{
+    setSuccess(l_name);
+  };
+  
 
   if(birthdateValue === ''){
-    setError(birthdate, 'Veiller renseigner le ddn');
+    setError(birthdate, 'Veillez renseigner le ddn');
   }else{
     setSuccess(birthdate);
   }
@@ -114,24 +149,6 @@ else if(!validerName(l_nameValue)){
   }
 }
 
-// Task #3 Validation des champs <input> via l'API de validation JS pour utiliser les contraintes HTML
-// par ajout d'un attribut (utilisation du CSS fourni)
-// Sauf pour les champs Prénom, Nom et E-mail pas suffisemment fiables (utilisation de REGEX)
 
-function validerName(l_name) {
-  let regexName = /^[a-zA-Z]+(([- ])?[a-zA-Z])+$/;
-  return regexName.test(l_name);
-};
+  
 
-// Validation adresse mail par RegEx, plus fiable que le contrôle HTML
-function validerEmail(email) {
-  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-};
-
-// function ControleBithdate() {
-//   if (birthdate.validity.valid) {
-//     setError(birthdate, 'Veuillez entrer une date valide');
-    
-//   };
-// };
